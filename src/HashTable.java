929d9028fd9b9915179232a244424902f1b8771d
rubbishChicken <guoguoruiguoguorui@hotmail.com>
2018-03-10 23:17:08 +0800

    hashtable map

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,6 +1,6 @@
-public class HashTable<T>{
+public class HashTable<K,V>{
     int capacity=16;
-    LinkedList[] linkedLists;
+    HashLinkedList[] linkedLists;
     int numOfNodes;
     int hashShift=4;
 
@@ -8,7 +8,7 @@ public class HashTable<T>{
         if(initCapacity<=0)
             throw new RuntimeException("sorry you call the wrong number");
         this.capacity=capacity;
-        linkedLists=new LinkedList[capacity];
+        linkedLists=new HashLinkedList[capacity];
         hashShift=0;
         while((1<<hashShift)<=capacity){
             hashShift++;
@@ -16,17 +16,17 @@ public class HashTable<T>{
     }
 
     HashTable(){
-        linkedLists=new LinkedList[capacity];
+        linkedLists=new HashLinkedList[capacity];
     }
 
-    int hash(T value){
-        return value.hashCode() & ((1<<hashShift)-1);
+    int hash(K key){
+        return key.hashCode() & ((1<<hashShift)-1);
     }
 
-    void insertOnHashTable(T value){
-        int insertIndex=hash(value);
+    void insertOnHashTable(K key,V value){
+        int insertIndex=hash(key);
         hashShift++;
-        linkedLists[insertIndex].insertToHead(value);
+        linkedLists[insertIndex].insertToHead(key,value);
         numOfNodes++;
         if(numOfNodes>capacity*0.75){
             resize();
@@ -35,13 +35,13 @@ public class HashTable<T>{
 
     void resize(){
         int newCapacity=capacity*2;
-        LinkedList[] newLinkedLists=new LinkedList[newCapacity];
+        HashLinkedList[] newLinkedLists=new HashLinkedList[newCapacity];
         int shiftFlag;
         for(int i=0;i<linkedLists.length;i++){
-            LinkedList originList=null,shiftList=null;
-            Node currentNode=linkedLists[i].head;
+            HashLinkedList originList=null,shiftList=null;
+            HashNode currentNode=linkedLists[i].head;
             while(currentNode!=null){
-                shiftFlag=currentNode.val.hashCode() & (1<< hashShift);
+                shiftFlag=currentNode.key.hashCode() & (1<< hashShift);
                 if(shiftFlag!=0)
                     shiftList.insertToHead(currentNode);
                 else
@@ -53,13 +53,87 @@ public class HashTable<T>{
         linkedLists=newLinkedLists;
     }
 
-    Node findOnHashTable(T value){
-        return linkedLists[hash(value)].find(value);
+    V findOnHashTable(K key){
+        return (V)linkedLists[hash(key)].find(key);
     }
 
-    void deleteOnHashTable(T value){
-        linkedLists[hash(value)].delete(value);
+    void deleteOnHashTable(K key){
+        linkedLists[hash(key)].delete(key);
     }
 
 }
+
+class HashNode<K,V> {
+    K key;
+    V value;
+    HashNode next;
+    HashNode(K key,V value){
+        this.key=key;
+        this.value=value;
+    }
+}
+
+class HashLinkedList<K,V>{
+
+    HashNode head;
+
+    V find(K key){
+        HashNode tempNode=head;
+        while(tempNode!=null){
+            if((Integer)tempNode.key==key)
+                return (V)tempNode.value;
+            tempNode=tempNode.next;
+        }
+        return null;
+    }
+
+    void delete(K key){
+        HashNode tempNode=head,previousNode=head;
+        while(tempNode!=null){
+            if((Integer)tempNode.key==key){
+                if(previousNode.next==null)
+                    head=null;
+                previousNode.next=tempNode.next;
+                break;
+            }
+            previousNode=tempNode;
+            tempNode=tempNode.next;
+        }
+    }
+
+    void insert(HashNode previous, K key,V value){
+        HashNode newNode=new HashNode<>(key,value);
+        if(previous!=null){
+            newNode.next=previous.next;
+            previous.next=newNode;
+        }
+        else{
+            head=newNode;
+        }
+    }
+
+    void insertToHead(K key,V value){
+        if(head==null)
+            head=new HashNode<>(key,value);
+        else{
+            HashNode newHead=new HashNode<>(key,value);
+            newHead.next=head;
+            head=newHead;
+        }
+    }
+
+    void insertToHead(HashNode node){
+        if(head==null)
+            head=node;
+        else{
+            node.next=head;
+            head=node;
+        }
+    }
+
+}
+
+
+
+
 
